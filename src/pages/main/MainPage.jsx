44199aa2262b73121f30ae9e8ab2d276628c5583
zhaobowen <1964471757@qq.com>
2022-04-23 21:10:47 +0800

    添加静态文件

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-// import styles from './MainPage.module.css';
+import React, { useEffect, useRef } from 'react'
+import styles from './MainPage.module.css';
 import { Header } from '../../components/header';
+import startBtn from '../../assets/start-btn.png'
+import lottie from 'lottie-web';
+import mainJson from '../../assets/lottie/cg_main.json';
 
 export function MainPage() {
+  const couterRef = useRef(null);
+  useEffect(() => {
+    console.log('couterRef', mainJson);
+    lottie.loadAnimation({
+      container: couterRef.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: mainJson,
+      assetsPath: '/lottie/earth-animation/images'
+    });
+  }, [])
   return (
     <>
       <Header/>
-      {/* <div className={styles["page-content"]}>
-        main
-      </div> */}
+      <div className={styles["page-content"]}>
+        <div className={styles["cryptoGravity-wrap"]}>
+          <div ref={couterRef}></div>
+          <div className={styles["cryptoGravity-content"]}>
+            <div className={styles["cryptoGravity-title"]}>CryptoGravity</div>
+            <div className={styles["cryptoGravity-desc"]}>Building the next-generation intelligent application cloud service platform</div>
+            <div className={styles['start-btn']}>
+              <span>Start</span>
+              <img className={styles['start-btn-img']} src={startBtn} alt="startBtn" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
